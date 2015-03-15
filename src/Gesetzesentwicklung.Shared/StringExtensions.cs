fcc909116fde689ad69dee712ed05304abde1f81
Gregor Dschung <mail@dschung.de>
2015-03-15 13:53:33 +0100

    Neues Projekt Gesetzesentwicklung.Shared für projektübergreifende Programmlogik

---
+++ src/Gesetzesentwicklung.Shared/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.Shared
+{
+    public static class StringExtensions
+    {
+
+        // Quelle: http://stackoverflow.com/questions/323640/can-i-convert-a-c-sharp-string-value-to-an-escaped-string-literal
+        public static string ToLiteral(this string input)
+        {
+            using (var writer = new StringWriter())
+            {
+                using (var provider = CodeDomProvider.CreateProvider("CSharp"))
+                {
+                    provider.GenerateCodeFromExpression(new CodePrimitiveExpression(input), writer, null);
+                    return writer.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ test/Gesetzesentwicklung.Shared.Tests/StringExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.Shared.Tests
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void Shared_StringExtensions_ToLiteral()
+        {
+            var str = @"Zeile 1
+Zeile 2.";
+
+            Assert.That(str.ToLiteral(), Is.EqualTo(@"""Zeile 1\r\nZeile 2."""));
+        }
+    }
+}
