d431e9bbca2303abe5c2f211df02bbb0ea5aec2e
Gregor Dschung <mail@dschung.de>
2015-03-24 00:11:34 +0100

    Erste Gui-Experimente

---
+++ src/Gesetzesentwicklung.GUI/ViewModels/GIISelectorViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.GUI.ViewModels
+{
+    public class GIISelectorViewModel : PropertyChangedBase
+    {
+        private string _test;
+
+        public string Test
+        {
+            get { return _test; }
+            set
+            {
+                _test = value;
+            }
+        }
+    }
+}
---
+++ src/Gesetzesentwicklung.GUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.GUI.ViewModels
+{
+    class MainViewModel : PropertyChangedBase
+    {
+        private string _windowTitle = "Default Title";
+
+        private GIISelectorViewModel _giiSelectorView;
+
+        public MainViewModel()
+        {
+            _giiSelectorView = new GIISelectorViewModel();
+        }
+
+        public string WindowTitle
+        {
+            get { return _windowTitle; }
+            set
+            {
+                _windowTitle = value;
+                NotifyOfPropertyChange(() => WindowTitle);
+            }
+        }
+
+        public GIISelectorViewModel GIISelectorView
+        {
+            get { return _giiSelectorView; }
+            set
+            {
+                _giiSelectorView = value;
+                NotifyOfPropertyChange(() => GIISelectorView);
+            }
+        }
+    }
+}
