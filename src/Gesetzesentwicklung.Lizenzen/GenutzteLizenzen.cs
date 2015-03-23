3210f68436b75f2f760313a1a1cd4ee1ce6159c9
Gregor Dschung <mail@dschung.de>
2015-03-24 00:10:45 +0100

    Models für die Lizenz-Dateien

---
+++ src/Gesetzesentwicklung.Lizenzen/GenutzteLizenzen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace Gesetzesentwicklung.Lizenzen
+{
+    public class GenutzteLizenzen
+    {
+        public IEnumerable<Lizenz> Lizenzen { get; private set; }
+
+        public GenutzteLizenzen()
+        {
+            Lizenzen = GetLizenzenFromResources();
+        }
+
+        private IEnumerable<Lizenz> GetLizenzenFromResources()
+        {
+            var deserializer = new Deserializer();
+            var lizenzResourcen = from resource in Assembly.GetExecutingAssembly().GetManifestResourceNames()
+                                  where resource.StartsWith("Gesetzesentwicklung.Lizenzen.Lizenzen") && resource.EndsWith(".yaml")
+                                  select resource;
+
+            foreach (var lizenzResource in lizenzResourcen)
+            {
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(lizenzResource))
+                {
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        yield return deserializer.Deserialize<Lizenz>(streamReader);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/Gesetzesentwicklung.Lizenzen.Tests/LizenzenTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.Lizenzen.Tests
+{
+    [TestFixture]
+    public class LizenzenTests
+    {
+        private GenutzteLizenzen _lizenzen;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _lizenzen = new GenutzteLizenzen();
+        }
+
+        [Test]
+        public void Lizenzen_AusgabeDerGenutztenLizenzen()
+        {
+            var ausgabe = from lizenz in _lizenzen.Lizenzen
+                          select string.Format(@"Projekt: {0}
+Autor: {1}
+Homepage: {2}
+Lizenz:
+{3}", lizenz.Projekt, lizenz.Autor, lizenz.Homepage, lizenz.LizenzText);
+
+            Console.WriteLine(string.Join(Environment.NewLine, ausgabe.ToArray()));
+        }
+    }
+}
