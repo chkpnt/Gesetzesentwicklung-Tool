bbdc2da1e53587eb77bf65e542160c13baab8a33
Gregor Dschung <mail@dschung.de>
2015-03-26 00:34:05 +0100

    Inhaltsverzeichnis aus dem Netz laden

---
+++ test/Gesetzesentwicklung.GII.Tests/VerzeichnisLaderTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.GII.Tests
+{
+    [TestFixture]
+    public class VerzeichnisLaderTests
+    {
+        [Test]
+        [Category("WithExternalResource")]
+        public async void GII_VerzeichnisLader()
+        {
+            var lader = new VerzeichnisLader();
+            var xmlVerzeichnis = await lader.LadeVerzeichnis();
+            Assert.That(xmlVerzeichnis.Normen, Has.Count.GreaterThan(0));
+        }
+    }
+}
