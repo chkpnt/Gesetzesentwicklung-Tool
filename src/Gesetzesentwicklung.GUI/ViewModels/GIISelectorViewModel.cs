61088afa3213095b48edaa8f994b820d471370ae
Gregor Dschung <mail@dschung.de>
2015-03-25 23:00:07 +0100

    Weiterentwicklung der GUI

--- src/Gesetzesentwicklung.GUI/ViewModels/GIISelectorViewModel.cs
+++ src/Gesetzesentwicklung.GUI/ViewModels/GIISelectorViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,37 @@ namespace Gesetzesentwicklung.GUI.ViewModels
 {
     public class GIISelectorViewModel : PropertyChangedBase
     {
-        private string _test;
+        private string _gesetzesFilter;
 
-        public string Test
+        private ObservableCollection<string> _gesetzeImInternet;
+
+        public string GesetzesFilter
         {
-            get { return _test; }
+            get { return _gesetzesFilter; }
             set
             {
-                _test = value;
+                _gesetzesFilter = value;
+                NotifyOfPropertyChange(() => GesetzesFilter);
             }
+        }
+
+        public ObservableCollection<string> GesetzeImInternet
+        {
+            get { return _gesetzeImInternet; }
+            set
+            {
+                _gesetzeImInternet = value;
+                NotifyOfPropertyChange(() => GesetzeImInternet);
+            }
+        }
+
+        public GIISelectorViewModel()
+        {
+            _gesetzesFilter = "Bla blub";
+            _gesetzeImInternet = new ObservableCollection<string>
+            {
+                "bla", "blub", "foobar"
+            };
         }
     }
 }
