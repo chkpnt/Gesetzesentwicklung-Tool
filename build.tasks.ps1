3015168e4e35d2d9ba1ef33429f58d672c187d9a
Gregor Dschung <mail@dschung.de>
2015-04-04 00:38:55 +0200

    Umstellung auf psake und Einbindung von Codeabdeckung

---
+++ build.tasks.ps1
@@ -0,0 +1,83 @@
+properties {
+  $codecoverage = "VisualStudio"
+  
+  $vstest_console_exe = Resolve-Path ($env:VS120COMNTOOLS + "..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe")
+  $coveralls_exe = Resolve-Path "packages\coveralls.net.*\csmacnz.coveralls.exe"
+  
+  if ($codecoverage -eq "VisualStudio") {
+    $code_coverage_exe = Resolve-Path ($env:VS120COMNTOOLS + "..\..\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe")
+  }
+  
+  $test_results_dir = "TestResults\"
+  
+  $test_assembly_pattern = "test\**\*.Tests.dll"
+  
+  $sln_file = "Gesetzesentwicklung-Tool.sln"
+  $coverage_xml = "coverage.coveragexml"
+  
+  $test_assemblies = @()
+}
+
+task default
+
+task RestoreNuGetPackages {
+  exec {
+    nuget.exe restore $sln_file
+  }
+}
+
+task Clean {
+  if (Test-Path $test_results_dir) {
+    Remove-Item $test_results_dir -r
+  }
+  
+  if (Test-Path $coverage_xml) {
+    Remove-Item $coverage_xml
+  }
+  
+  exec { msbuild "/t:Clean" $sln_file }
+}
+
+task Build -depends Clean {
+  exec { msbuild $sln_file }
+}
+
+task FindTestAssemblies {
+  $script:test_assemblies = @(Get-ChildItem "test\**\*.Tests.dll" -Recurse)
+}
+
+task Test -depends FindTestAssemblies {
+  exec {
+    & $vstest_console_exe /InIsolation /EnableCodeCoverage /Settings:CodeCoverage.runsettings /TestAdapterPath:.\packages\NUnitTestAdapter.2.0.0\lib\ $script:test_assemblies
+  }
+  
+  if ($codecoverage -eq "VisualStudio") {
+    $coverage_file = Resolve-Path -path "TestResults\*\*.coverage"
+    exec {
+      Write-Host ("Analyzing " + $coverage_file + " and saving as " + $coverage_xml)
+      & $code_coverage_exe analyze /output:$coverage_xml "$coverage_file"
+    }
+  }
+}
+
+task AppVeyor-PublishCodeCoverage {
+  Write-Host $codecoverage
+  
+  if (!(Test-Path $coverage_xml)) {
+    throw ("Code-Coverage-Datei fehlt: " + $coverage_xml)
+  }
+  
+  exec {
+    & $coveralls_exe --dynamiccodecoverage `
+                     -i coverage.coveragexml `
+                     -o coverallsTestOutput.json `
+                     --repoToken $env:coveralls_token `
+                     --useRelativePaths `
+                     --commitId $env:APPVEYOR_REPO_COMMIT `
+                     --commitBranch $env:APPVEYOR_REPO_BRANCH `
+                     --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR `
+                     --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL `
+                     --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE `
+                     --jobId $env:APPVEYOR_JOB_ID
+  }
+}
