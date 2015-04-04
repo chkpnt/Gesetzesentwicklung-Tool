73b3ca38b171fcb27a6752594406b37f6d83b614
Gregor Dschung <mail@dschung.de>
2015-04-04 22:35:20 +0200

    Mehr Tests...

---
+++ test/Gesetzesentwicklung.Validators.Tests/ValidatorProtokollTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.Validators.Tests
+{
+    [TestFixture]
+    public class ValidatorProtokollTests
+    {
+        private ValidatorProtokoll _classUnderTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _classUnderTest = new ValidatorProtokoll();
+        }
+
+        [Test]
+        public void Validator_ValidatorProtokoll()
+        {
+            _classUnderTest.AddEntry("Ein Eintrag");
+            _classUnderTest.AddEntries(new List<string> { "foo", "bar" });
+
+            var expected = new List<string> { "Ein Eintrag", "foo", "bar" };
+            Assert.That(_classUnderTest.Entries, Is.EquivalentTo(expected));
+        }
+    }
+}
