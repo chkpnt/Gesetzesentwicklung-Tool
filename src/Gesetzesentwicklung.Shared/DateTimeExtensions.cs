862cc1e0e9623da060881018a1280f45530b50d6
Gregor Dschung <mail@dschung.de>
2015-04-15 22:44:07 +0200

    DateTime-Extension ToEpoch()

---
+++ src/Gesetzesentwicklung.Shared/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.Shared
+{
+    public static class DateTimeExtensions
+    {
+        private readonly static DateTime Epoch0 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long ToEpoch(this DateTime date)
+        {
+            return Convert.ToInt64((date.ToUniversalTime() - Epoch0).TotalSeconds);
+        }
+    }
+}
---
+++ test/Gesetzesentwicklung.Shared.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.Shared.Tests
+{
+    [TestFixture]
+    public class DateTimeExtensionsTests
+    {
+        [Test]
+        public void Shared_DateTimeExtensions_ToEpoch()
+        {
+            Assert.That(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToEpoch(), Is.EqualTo(0));
+            Assert.That(new DateTime(1970, 1, 2, 0, 0, 0, DateTimeKind.Utc).ToEpoch(), Is.EqualTo(86400));
+            Assert.That(DateTime.ParseExact("01.01.1970 00:00:01 -01:00", "dd.MM.yyyy HH:mm:ss zzz", null).ToEpoch(), Is.EqualTo(3601));
+        }
+    }
+}
