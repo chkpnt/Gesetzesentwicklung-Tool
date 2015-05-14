5365d7511bebe49dbb0106d3d30c298302552372
Gregor Dschung <mail@dschung.de>
2015-05-14 13:22:52 +0200

    DirectoryInfoExtension-Methode "CopyTo"

---
+++ src/Gesetzesentwicklung.Git/DirectoryInfoExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.Git
+{
+    internal static class DirectoryInfoExtensions
+    {
+        // Vielleicht lieber ins Shared-Projekt. Aber dann bekomme ich
+        // dort eine Abhängigkeit zu System.IO.Abstractions hinein...
+        public static void CopyTo(this DirectoryInfoBase source, DirectoryInfoBase target)
+        {
+            if (!target.Exists)
+            {
+                target.Create();
+            }
+
+            foreach (var fileInfo in source.GetFiles())
+            {
+                fileInfo.CopyTo(Path.Combine(target.FullName, fileInfo.Name), true);
+            }
+
+            foreach (var sourceSubDir in source.GetDirectories())
+            {
+                var targetSubDir = target.CreateSubdirectory(sourceSubDir.Name);
+                sourceSubDir.CopyTo(targetSubDir);
+            }
+        }
+    }
+}
---
+++ test/Gesetzesentwicklung.Git.Tests/DirectoryInfoExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using Gesetzesentwicklung.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesetzesentwicklung.Git.Tests
+{
+    [TestFixture]
+    public class DirectoryInfoExtensionsTests
+    {
+        private IFileSystem _fileSystem;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { @"c:\data\src\Gesetzesstand.yml", new MockFileData("bla") },
+                { @"c:\data\src\Gesetzesstand\", new MockDirectoryData()},
+                { @"c:\data\src\Gesetze\GG\Bundesgesetzblatt.yml", new MockFileData("") }
+            });
+        }
+
+        [Test]
+        public void Git_DirectoryInfoExtensionsTests_CopyTo()
+        {
+            var src = _fileSystem.DirectoryInfo.FromDirectoryName(@"c:\data\src");
+            var dest = _fileSystem.DirectoryInfo.FromDirectoryName(@"c:\data\dest");
+
+            src.CopyTo(dest);
+
+            Assert.True(dest.Exists);
+            Assert.That(dest.GetDirectories("*", SearchOption.AllDirectories).Count(), Is.EqualTo(3));
+            Assert.True(_fileSystem.Directory.Exists(@"c:\data\dest\Gesetzesstand"));
+            Assert.True(_fileSystem.Directory.Exists(@"c:\data\dest\Gesetze"));
+            Assert.True(_fileSystem.Directory.Exists(@"c:\data\dest\Gesetze\GG"));
+            Assert.That(dest.GetFiles("*", SearchOption.AllDirectories).Count(), Is.EqualTo(2));
+            Assert.True(_fileSystem.File.Exists(@"c:\data\dest\Gesetzesstand.yml"));
+            Assert.True(_fileSystem.File.Exists(@"c:\data\dest\Gesetze\GG\Bundesgesetzblatt.yml"));
+        }
+    }
+}
