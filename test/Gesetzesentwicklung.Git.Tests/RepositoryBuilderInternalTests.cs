0dcc2050d1f5e09549a9b24cf19523b6c941c273
Gregor Dschung <mail@dschung.de>
2015-05-17 17:47:06 +0200

    Umstrukturierung der RepositoryBuilder-Tests

--- test/Gesetzesentwicklung.Git.Tests/RepositoryBuilderInternalTests.cs
+++ test/Gesetzesentwicklung.Git.Tests/RepositoryBuilderInternalTests.cs
@@ -14,15 +14,64 @@ namespace Gesetzesentwicklung.Git.Tests
     [TestFixture]
     public class RepositoryBuilderInternalTests
     {
-        private DirectoryInfoBase _srcDir = new DirectoryInfo(@"c:\data\src");
+        private DirectoryInfoBase _sourceDirInfo;
+        private DirectoryInfoBase _destDirInfo;
+
+        private IFileSystem _fileSystem;
 
         private RepositoryBuilder _classUnderTest;
-
+        
         [SetUp]
         public void SetUp()
         {
-            var fileSystem = new MockFileSystem();
-            _classUnderTest = new RepositoryBuilder(fileSystem);
+            _fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { @"c:\data\GesetzesData\Gesetzesstand.yml", new MockFileData(
+@"Commits:
+- Daten: Gesetzesstand
+  Ziel: /
+  Autor: Foo Bar <foo@example.net>
+  Datum: 01.01.1980
+  Beschreibung: |-
+    1. Lesung
+
+    Letzte Zeile") },
+                { @"c:\data\GesetzesData\Gesetzesstand\", new MockDirectoryData()},
+                { @"c:\data\GesetzesData\Gesetze\GG\Bundesgesetzblatt.yml", new MockFileData(
+@"AutoMergeInto: Gesetzesstand
+Commits:
+- BranchFrom: Gesetzesstand
+  Autor: Foo Bar <foo@example.net>
+  Datum: 01.01.1981
+  Beschreibung: init") },
+                { @"c:\data\GesetzesData\Gesetze\GG\Änderung-1.yml", new MockFileData(
+@"Commits:
+- Daten: Änderung-1\Lesung-2
+  Ziel: /GG
+  MergeInto: Gesetze/GG/Bundesgesetzblatt
+  Autor: Foo Bar <foo@example.net>
+  Datum: 10.04.1982
+  Beschreibung: |-
+    2. Lesung
+
+    Letzte Zeile
+- Daten: Änderung-1\Lesung-1
+  Ziel: /GG
+  BranchFrom: Gesetze/GG/Bundesgesetzblatt
+  Autor: Foo Bar <foo@example.net>
+  Datum: 01.01.1982
+  Beschreibung: |-
+    1. Lesung
+
+    Letzte Zeile") },
+                { @"c:\data\GesetzesData\Gesetze\GG\Änderung-1\Lesung-1\", new MockDirectoryData()},
+                { @"c:\data\GesetzesData\Gesetze\GG\Änderung-1\Lesung-2\", new MockDirectoryData()},
+            });
+
+            _sourceDirInfo = _fileSystem.DirectoryInfo.FromDirectoryName(@"c:\data\GesetzesData\");
+            _destDirInfo = _fileSystem.DirectoryInfo.FromDirectoryName(@"c:\data\GesetzesRepo\");
+
+            _classUnderTest = new RepositoryBuilder(_fileSystem);
         }
 
         [TestCase(null)]
@@ -31,7 +80,7 @@ namespace Gesetzesentwicklung.Git.Tests
          ExpectedMessage = "Branch kann nicht abgeleitet werden: Fehlender Dateiname")]
         public void Git_RepositoryBuilder_DeriveBranchName_Fehler(string filename)
         {
-            _classUnderTest.DeriveBranchName(null, _srcDir);
+            _classUnderTest.DeriveBranchName(null, _sourceDirInfo);
         }
 
         [TestCase(@"c:\data\foobar\Gesetzesstand.yml")]
@@ -40,11 +89,52 @@ namespace Gesetzesentwicklung.Git.Tests
          MatchType = MessageMatch.StartsWith)]
         public void Git_RepositoryBuilder_DeriveBranchName_SettingsDateiNichtUnterhalbDesSrcDirs(string filename)
         {
-            _classUnderTest.DeriveBranchName(filename, _srcDir);
+            _classUnderTest.DeriveBranchName(filename, _sourceDirInfo);
         }
 
-        [TestCase(@"c:\data\src\Gesetzesstand.yml", Result = "Gesetzesstand")]
-        [TestCase(@"c:\data\src\GG\Blub\Änderung-1.yml", Result = "GG/Blub/Änderung-1")]
-        public string Git_RepositoryBuilder_DeriveBranchName(string filename) => _classUnderTest.DeriveBranchName(filename, _srcDir);
+        [TestCase(@"c:\data\GesetzesData\Gesetzesstand.yml", Result = "Gesetzesstand")]
+        [TestCase(@"c:\data\GesetzesData\GG\Blub\Änderung-1.yml", Result = "GG/Blub/Änderung-1")]
+        public string Git_RepositoryBuilder_DeriveBranchName(string filename) => _classUnderTest.DeriveBranchName(filename, _sourceDirInfo);
+
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException),
+         ExpectedMessage = @"^Verzeichnis existiert nicht: c:\\data\\gibtsNicht$",
+         MatchType = MessageMatch.Regex)]
+        public void Git_RepositoryBuilder_TestAssertions_NichtExistierendesQuellVerzeichnis()
+        {
+            var sourceDirInfo = _fileSystem.DirectoryInfo.FromDirectoryName(@"c:\data\gibtsNicht\");
+
+            _classUnderTest.TestAssertions(sourceDirInfo, _destDirInfo);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException),
+         ExpectedMessage = "Quell- und Zielverzeichnisse dürfen nicht gleich sein")]
+        public void Git_RepositoryBuilder_TestAssertions_GleichesQuellUndZielverzeichnis()
+        {
+            _classUnderTest.TestAssertions(_sourceDirInfo, _sourceDirInfo);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException),
+         ExpectedMessage = @"^Verzeichnis existiert schon: c:\\data\\GesetzesRepo$",
+         MatchType = MessageMatch.Regex)]
+        public void Git_RepositoryBuilder_TestAssertions_ZielverzeichnisExistiertSchon()
+        {
+            _destDirInfo.Create();
+
+            _classUnderTest.TestAssertions(_sourceDirInfo, _destDirInfo);
+        }
+
+        [Test]
+        public void Git_RepositoryBuilder_ReadCommitSettings_CommitSettingsDateienKorrektEingelesen()
+        {
+            var commitSettings = _classUnderTest.ReadCommitSettings(_sourceDirInfo);
+
+            Assert.That(commitSettings.Commits.Count(), Is.EqualTo(4));
+            Assert.That(commitSettings.Commits.First()._Datum, Is.EqualTo("01.01.1980"));
+            Assert.That(commitSettings.Commits.Last()._Datum, Is.EqualTo("10.04.1982"));
+        }
     }
 }
